c23b62479fb6e03a045043ca5788cba223185690
y-murata <mura123yasu@gmail.com>
2021-05-18 00:34:45 +0900

    feat:show playback icon

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -17,8 +17,37 @@ class BoringPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('This is sooooo boring ...'),
+        child: PlaybackButton(),
       ),
+    );
+  }
+}
+
+class PlaybackButton extends StatefulWidget {
+  @override
+  _PlaybackButtonState createState() => _PlaybackButtonState();
+}
+
+
+class _PlaybackButtonState extends State<PlaybackButton> {
+  bool _isPlaying = false;
+
+  void _stop() {}
+
+  void _play() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: _isPlaying ? Icon(Icons.stop) : Icon(Icons.play_arrow),
+      onPressed: () {
+        if (_isPlaying) {
+          _stop();
+        } else {
+          _play();
+        }
+        _isPlaying = !_isPlaying;
+      },
     );
   }
 }
